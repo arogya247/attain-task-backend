c1bd0f746e01f5cb33d30c0bd9a76d9f1906fd5f
arogya247 <arogya247@gmail.com>
2025-06-15 03:46:27 +0530

    create trasactions functionality added

--- src/controllers/transactions.ts
+++ src/controllers/transactions.ts
@@ -4,6 +4,29 @@ import { Transaction } from '../models/Transaction';
 import { v4 as uuidv4 } from 'uuid';
 
 
+export const createTransaction = (req: Request, res: Response): void => {
+
+  const { userId, payee, amount, category, date } = req.body;
+
+  if (!userId || !payee || !amount || !category || !date) {
+    res.status(400).json({ error: 'Missing required fields' });
+    return
+  }
+
+  const newTx: Transaction = {
+    id: uuidv4(),
+    userId,
+    payee,
+    amount,
+    category,
+    date,
+    deleted: false,
+  };
+
+  transactions.push(newTx);
+  res.status(201).json(newTx);
+};
+
 export const getTransactions = (_req: Request, res: Response): void => {
   const activeTxs = transactions.filter(tx => !tx.deleted);
   res.json(activeTxs);
--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import {
+  createTransaction,
   getTransactions,
 } from '../controllers/transactions';
 
 const router = express.Router();
 
 router.get('/', getTransactions);
+router.post('/', createTransaction);
 
 
 export default router;
