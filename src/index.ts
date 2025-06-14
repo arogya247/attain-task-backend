1693af61e7a4e422fa49ed20d59e5b5468cbf483
arogya247 <arogya247@gmail.com>
2025-06-15 04:34:57 +0530

    cors error handled

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import transactionRoutes from './routes/transactions';
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
 
 // transaction routes
 app.use('/transactions', transactionRoutes);
